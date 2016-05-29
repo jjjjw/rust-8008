2056c2d9d02bf77c31cd5d2f0bcba2e14383f21b
jj <jj.weber@gmail.com>
2016-05-29 13:53:50 -0700

    Allow osc to use any waveform

--- src/synth.rs
+++ src/synth.rs
@@ -1,42 +1,40 @@
 use dsp::{Node, Settings};
-use waveforms::{Waveform, Sine};
+use waveforms::{Waveform, amp_at_phase, Phase, Amp};
 
 pub type Frequency = f64;
-pub type Phase = f64;
 pub type Output = f32; // f64 not supported yet
-pub type Amp = f64;
 pub type Volume = f64;
 
 pub struct Oscillator {
   phase: Phase,
-  waveform: Sine // TODO: any waveform
+  waveform: Waveform
 }
 
 pub struct Synth {
-  oscillator: Oscillator,
   frequency: Frequency,
   volume: Amp,
+  oscillator: Oscillator,
 }
 
 impl Oscillator {
-    pub fn new(waveform: Sine) -> Oscillator {
+    pub fn new(waveform: Waveform) -> Oscillator {
         Oscillator {
             phase: 0.0,
             waveform: waveform
         }
     }
 
-    pub fn amp_at_next_phase(&mut self, frequency: Frequency, settings: Settings) -> Amp {
-        let val = self.waveform.amp_at_phase(self.phase);
+    pub fn tick(&mut self, frequency: Frequency, settings: Settings) -> Amp {
+        let amp = amp_at_phase(&self.waveform, self.phase);
         self.phase += frequency / settings.sample_hz as f64;
-        val
+        amp
     }
 }
 
 impl Synth {
     pub fn new(oscillator: Oscillator, frequency: Frequency, volume: Amp) -> Synth {
         Synth {
-            oscillator: oscillator, // TODO: multiple oscillator
+            oscillator: oscillator, // TODO: multiple.dynamic oscillators
             frequency: frequency,  // TODO: dynamic frequency
             volume: volume  // TODO: dynamic volume
         }
@@ -47,7 +45,7 @@ impl Node<Output> for Synth {
     /// Here we'll override the audio_requested method and generate a sound.
     fn audio_requested(&mut self, buffer: &mut [Output], settings: Settings) {
         for frame in buffer.chunks_mut(settings.channels as usize) {
-            let amp = self.oscillator.amp_at_next_phase(self.frequency, settings);
+            let amp = self.oscillator.tick(self.frequency, settings);
             for channel in frame.iter_mut() {
                 *channel = (amp * self.volume) as f32;
             }
@@ -56,5 +54,5 @@ impl Node<Output> for Synth {
 }
 
 pub fn sine_synth (frequency: Frequency) -> Synth {
-    Synth::new(Oscillator::new(Sine), frequency, 0.15)
+    Synth::new(Oscillator::new(Waveform::Square), frequency, 0.15)
 }
--- src/waveforms.rs
+++ src/waveforms.rs
@@ -1,64 +1,20 @@
-/// Some type that can return an amplitude given some phase.
-pub trait Waveform {
-    /// Return the amplitude given some phase.
-    fn amp_at_phase(&self, phase: f64) -> f64;
-}
-
-/// Twice PI.
 const PI_2: f64 = ::std::f64::consts::PI * 2.0;
 
-/// Represents the "steepness" of the exponential saw wave.
-pub type Steepness = f64;
+pub type Phase = f64;
+pub type Amp = f64;
 
-/// A sine wave.
-#[derive(Copy, Clone, Debug)]
-pub struct Sine;
-
-/// A sawtooth wave.
-#[derive(Copy, Clone, Debug)]
-pub struct Saw;
-
-/// An exponential sawtooth wave.
-#[derive(Copy, Clone, Debug)]
-pub struct SawExp(pub Steepness);
-
-/// A square wave.
-#[derive(Copy, Clone, Debug)]
-pub struct Square;
-
-/// A noise signal.
-#[derive(Copy, Clone, Debug)]
-pub struct Noise;
-
-impl Waveform for Sine {
-    fn amp_at_phase(&self, phase: f64) -> f64 {
-        (PI_2 * phase).sin() as f64
-    }
+pub enum Waveform {
+    Sine,
+    Saw,
+    Square,
+    Noise
 }
 
-impl Waveform for Saw {
-    fn amp_at_phase(&self, phase: f64) -> f64 {
-        (::utils::fmod(phase, 1.0) * -2.0 + 1.0) as f64
-    }
-}
-
-impl Waveform for SawExp {
-    fn amp_at_phase(&self, phase: f64) -> f64 {
-        let SawExp(steepness) = *self;
-        let saw = Saw.amp_at_phase(phase);
-        saw * saw.abs().powf(steepness)
-    }
-}
-
-impl Waveform for Square {
-    fn amp_at_phase(&self, phase: f64) -> f64 {
-        (if ::utils::fmod(phase, 1.0) < 0.5 { -1.0 } else { 1.0 }) as f64
-        //(if (PI_2 * phase).sin() < 0.0 { -1.0 } else { 1.0 }) as f64
-    }
-}
-
-impl Waveform for Noise {
-    fn amp_at_phase(&self, _phase: f64) -> f64 {
-        ::rand::random::<f64>() * 2.0 - 1.0
+pub fn amp_at_phase(waveform: &Waveform, phase: Phase) -> Amp {
+    match *waveform {
+        Waveform::Sine => { (PI_2 * phase).sin() },
+        Waveform::Saw => { ::utils::fmod(phase, 1.0) * -2.0 + 1.0 },
+        Waveform::Square => { (if ::utils::fmod(phase, 1.0) < 0.5 { -1.0 } else { 1.0 }) },
+        Waveform::Noise => { ::rand::random::<f64>() * 2.0 - 1.0 }
     }
 }
