ca0d0adab6d3c48e6549e194036354ebd610b4b7
jj <jj.weber@gmail.com>
2016-08-09 10:27:57 -0700

    Implement using frame iterators over a concrete audio out type

--- src/drum_machine.rs
+++ src/drum_machine.rs
@@ -1,12 +1,45 @@
-use dsp::sample::Frame;
 use dsp::Node;
+use dsp::sample::frame::Stereo;
+use dsp::sample::signal;
+use dsp::sample::{Frame, Signal, slice};
 
 pub type Hz = f64;
-pub type Volume = f64;
+pub type PadIdx = usize;
 pub type Velocity = f64;
+pub type Volume = f64;
+pub type AudioOut = Stereo<f64>;
 
-enum Pad {
-    Input1,
+struct PadState {
+    /// Is the pad active.
+    active: bool,
+    /// The velocity this pad was triggered with.
+    vel: Velocity,
+}
+
+impl PadState {
+    fn new() -> Self {
+        PadState {
+            active: false,
+            vel: 0.0,
+        }
+    }
+
+    /// Trigger a sound.
+    fn trigger(&mut self, vel: Velocity) {
+        self.active = true;
+        self.vel = vel;
+    }
+
+    /// Deactivate.
+    fn silence(&mut self) {
+        self.active = false;
+        self.vel = 0.0;
+    }
+
+    /// Get the next audio frame.
+    fn next_frame(&mut self, sample_hz: Hz) -> AudioOut {
+        AudioOut::equilibrium()
+    }
 }
 
 pub struct Machine {
@@ -14,8 +47,8 @@ pub struct Machine {
     pub volume: Volume,
     /// Render playback or not.
     pub is_paused: bool,
-    /// The sample rate
-    pub sample_hz: Hz,
+    /// The states of the pads that generate sound for the machine.
+    pads: Vec<PadState>,
 }
 
 impl Machine {
@@ -24,15 +57,23 @@ impl Machine {
         Machine {
             volume: 1.0,
             is_paused: false,
-            sample_hz: 44_100.0,
+            pads: vec![PadState::new()],
         }
     }
 
     /// Trigger a pad.
-    pub fn trigger<T>(&mut self, pad: Pad, vel: Velocity) {}
+    pub fn trigger(&mut self, pad: PadIdx, vel: Velocity) {
+        if pad < self.pads.len() {
+            self.pads[pad].trigger(vel);
+        }
+    }
 
     /// Deactivate a pad.
-    pub fn silence<T>(&mut self, pad: Pad) {}
+    pub fn silence(&mut self, pad: PadIdx) {
+        if pad < self.pads.len() {
+            self.pads[pad].silence();
+        }
+    }
 
     /// Pause playback.
     pub fn pause(&mut self) {
@@ -45,11 +86,21 @@ impl Machine {
     }
 
     /// Stop playback and clear the current pads.
-    pub fn stop(&mut self) {}
+    pub fn stop(&mut self) {
+        for pad in &mut self.pads {
+            pad.silence();
+        }
+    }
+
+    /// Get the next audio frame.
+    pub fn next_frame(&mut self, sample_hz: Hz) -> AudioOut {
+        self.pads.iter_mut().fold(AudioOut::equilibrium(),
+                                  |f, pad| f.add_amp(pad.next_frame(sample_hz)))
+    }
 }
 
-impl<F> Node<F> for Machine
-    where F: Frame
-{
-    fn audio_requested(&mut self, buffer: &mut [F], sample_hz: Hz) {}
+impl Node<AudioOut> for Machine {
+    fn audio_requested(&mut self, buffer: &mut [AudioOut], sample_hz: Hz) {
+        slice::map_in_place(buffer, |_| self.next_frame(sample_hz));
+    }
 }
